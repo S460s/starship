5429c4adf1cb4ab5e07c0e040f6d9158d4fa442a
S460 <rommel.e@protonmail.ch>
2022-05-27 22:14:08 +0300

    add constants in their own module

---
+++ consts.rb
@@ -0,0 +1,5 @@
+# Module containing various constants needed throughout the game
+module Constants
+  Z_INDEX = { BACKGROUND: 0, STARS: 1, PLAYER: 2, UI: 3 }.freeze
+  SCREE_SIZE = { width: 640, height: 480 }.freeze
+end
